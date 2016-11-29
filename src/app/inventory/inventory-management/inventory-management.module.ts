e5292d8f70498b0cee30b7665dc8a6cc4ed3a00e
BrianPhiri <brianphiri.9523@gmal.com>
2016-11-29 09:51:56 +0300

    added table for inventory listing

--- src/app/inventory/inventory-management/inventory-management.module.ts
+++ src/app/inventory/inventory-management/inventory-management.module.ts
@@ -2,15 +2,15 @@ import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InventoryManagementRouting } from './inventory-management.routing';
 import { InventoryManagementComponent } from './inventory-management.component';
-import {SmartadminModule} from "../../shared/smartadmin.module";
-import {SmartadminInputModule} from "../../shared/forms/input/smartadmin-input.module";
+import {SmartadminModule} from "../../shared/smartadmin.module";    
+import {SmartadminDatatableModule} from "../../shared/ui/datatable/smartadmin-datatable.module";
 
 @NgModule({
     imports: [
         CommonModule,
         InventoryManagementRouting,
         SmartadminModule,
-        SmartadminInputModule
+        SmartadminDatatableModule
     ],
     exports: [],
     declarations: [InventoryManagementComponent],
