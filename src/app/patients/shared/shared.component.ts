f7e71b06b3488882712a0424e17f05f0c56ec534
Brian Mwathi <Brian Mwathi>
2017-01-16 10:48:55 +0300

    Added contact column

--- src/app/patients/shared/shared.component.ts
+++ src/app/patients/shared/shared.component.ts
@@ -39,8 +39,8 @@ export class SharedComponent implements OnInit {
         buttonClasses: 'btn btn-default',
         selectionLimit: 0,
         closeOnSelect: false,
-        showCheckAll: false,
-        showUncheckAll: false,
+        showCheckAll: true,
+        showUncheckAll: true,
         dynamicTitleMaxItems: 3,
         maxHeight: '300px',
     };
@@ -70,54 +70,6 @@ export class SharedComponent implements OnInit {
     ngDoCheck(): void {
         this.model.bsa = Math.sqrt((this.model.current_weight * this.model.current_height) / 3600);
         // console.log(this.illness.data);
-    }
-
-    setDateOfBirth(value: any) {
-        let response = this._patientService.calculateDate(value);
-        this.model.birth_date = value;
-        this.model.age_in_years = response.years;
-        this.model.age_in_months = response.months;
-    }
-
-    setDate(value: any, val: string) {
-        if (val == 'tb_start') {
-            this.model.start_tb_phase = value;
-        }
-        if (val == 'tb_end') {
-            this.model.end_tb_phase = value;
-        }
-        if (val == 'enrollment') {
-            this.model.enrollment_date = value;
-        }
-        if (val == 'regimen') {
-            this.model.regimen_start_date = value;
-        }
-        if (val == 'isoniazid_start') {
-            this.model.isoniazid_start = value;
-        }
-        if (val == 'isoniazid_end') {
-            this.model.isoniazid_end = value;
-        }
-    }
-
-    setEnrollDate(value: any, val: string) {
-        this.model.enrollment_date = value;
-        console.log(val);
-    }
-
-    setregimenDate(value: any) {
-        this.model.regimen_start_date = value;
-    }
-
-    // TODO: Remove this when we're done
-
-    get diagnostic() {
-        return JSON.stringify(this.model);
-    }
-
-    onSubmit(): void {
-        this._patientService.addPatient(this.model);
-        // console.log(this.model);
     }
 
     /**
@@ -182,4 +134,59 @@ export class SharedComponent implements OnInit {
             'required': `Patient's height is required.`
         }
     };
+
+    /**
+     * Methods prefixed with set... modify the property values of
+     * the patient's model.
+     */
+
+    setDateOfBirth(value: any) {
+        let response = this._patientService.calculateDate(value);
+        this.model.birth_date = value;
+        this.model.age_in_years = response.years;
+        this.model.age_in_months = response.months;
+    }
+
+    setDate(value: any, val: string) {
+        if (val == 'tb_start') {
+            this.model.start_tb_phase = value;
+        }
+        if (val == 'tb_end') {
+            this.model.end_tb_phase = value;
+        }
+        if (val == 'enrollment') {
+            this.model.enrollment_date = value;
+        }
+        if (val == 'regimen') {
+            this.model.regimen_start_date = value;
+        }
+        if (val == 'isoniazid_start') {
+            this.model.isoniazid_start = value;
+        }
+        if (val == 'isoniazid_end') {
+            this.model.isoniazid_end = value;
+        }
+    }
+
+    setEnrollDate(value: any, val: string) {
+        this.model.enrollment_date = value;
+        console.log(val);
+    }
+
+    setregimenDate(value: any) {
+        this.model.regimen_start_date = value;
+    }
+
+    /**
+     * Submit form data to the back-end server
+     */
+    onSubmit(): void {
+        this._patientService.addPatient(this.model);
+    }
+
+    // TODO: Remove this when done
+    get diagnostic () {
+        return JSON.stringify(this.model);
+    }
+    
 }
--- src/app/shared/ui/datatable/datatable.component.ts
+++ src/app/shared/ui/datatable/datatable.component.ts
@@ -97,8 +97,9 @@ export class DatatableComponent implements OnInit {
             // NOTE: Targeting the [actions] column.
             "targets": 6
           },
-          { "visible": false, "targets": [3] }
-        ]
+          { "width": "10%", "targets": 0 }
+        ],       
+    responsive: true
       });
     } else if (this.tableType === "list-inventory") {
       options = $.extend(options, {
@@ -113,8 +114,7 @@ export class DatatableComponent implements OnInit {
             },
             // NOTE: Targeting the [actions] column.
             "targets": 7
-          },
-          { "visible": false, "targets": [3] }
+          }
         ]
       });
     }
