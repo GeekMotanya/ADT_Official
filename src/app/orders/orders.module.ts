1931560a1e6c281d92060c92435ed3402d7df398
[anamikoye] <[anamikoye52@gmail.com]>
2017-01-17 09:40:04 +0300

    Skeleton Structure + Routes

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
+import { UpdateCdrrComponent } from './update-cdrr/update-cdrr.component';
+import { UpdateMapComponent } from './update-map/update-map.component';
+import { ViewCdrrComponent } from './view-cdrr/view-cdrr.component';
+import { ViewMapComponent } from './view-map/view-map.component';
 import { routing } from "./orders.routing";
 import { SmartadminModule } from "../shared/smartadmin.module";
 import { FormsModule } from '@angular/forms';
@@ -8,7 +12,13 @@ import { SmartadminInputModule } from "../shared/forms/input/smartadmin-input.mo
 import { SmartadminDatatableModule } from "../shared/ui/datatable/smartadmin-datatable.module";
 
 @NgModule({
-  declarations: [OrdersComponent],
+  declarations: [
+    OrdersComponent,
+    ViewMapComponent,
+    ViewCdrrComponent,
+    UpdateMapComponent,
+    UpdateCdrrComponent
+  ],
   imports: [
     CommonModule,
     routing,
