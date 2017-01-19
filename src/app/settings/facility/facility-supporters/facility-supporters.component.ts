e053f8f363a2a9667883938ca0579b162811ce28
[anamikoye] <[anamikoye52@gmail.com]>
2017-01-19 13:25:02 +0300

    Facility 2.0

--- src/app/settings/facility/facility-supporters/facility-supporters.component.ts
+++ src/app/settings/facility/facility-supporters/facility-supporters.component.ts
@@ -9,7 +9,7 @@ export class FacilitySupportersComponent implements OnInit {
 
   constructor() { }
 
-   tableOptions: Object = {
+  tableOptions: Object = {
     colReorder: true,
     ajax: 'assets/api/tables/facility.dummy.json',
     columns: [{ data: 'supporter_name' }],
@@ -21,8 +21,26 @@ export class FacilitySupportersComponent implements OnInit {
         "render": function (data, type, row) {
           return `
                <div>
-               <button class="btn btn-primary"> Edit	</button>  
-               <button class="btn btn-primary"> Disable	</button>              
+               <button class="btn btn-primary" data-toggle="modal" data-target="#editSupporter"> Edit	</button>  
+               <button class="btn btn-primary"> Disable	</button>
+               <div id="editSupporter" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      <h4 class="modal-title">Edit Facility Supporter</h4>
+                    </div>
+                    <div class="modal-body">
+                      <form>
+                        <label>Client Support Name</label>
+                        <p><input type="text" class="form-control" style="width:100%"></p>
+                        <button class="btn btn-primary" type="submit"><i class="fa fa-save"></i> Update	</button>
+                        <button class="btn btn-primary" data-dismiss="modal"> Cancel	</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>              
               </div>
               `
           // return '<a class="btn btn-primary btn-xs" href="patients/dispense/' + row['id'] + '">Dispense</a> <a class="btn btn-primary btn-xs" href="patients/view/' + row['id'] + '">Detail</a>'
