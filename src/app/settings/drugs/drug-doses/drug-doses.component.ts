a393692011d3d29a6b32e1e5090bb83b32213aab
[anamikoye] <[anamikoye52@gmail.com]>
2017-01-19 17:09:13 +0300

    A bunch of drug views

--- src/app/settings/drugs/drug-doses/drug-doses.component.ts
+++ src/app/settings/drugs/drug-doses/drug-doses.component.ts
@@ -9,6 +9,27 @@ export class DrugDosesComponent implements OnInit {
 
   constructor() { }
 
+  tableOptions: Object = {
+    colReorder: true,
+    ajax: 'assets/api/tables/drugs.dummy.json',
+    columns: [{ data: 'dose' }, { data: 'value' }, { data: 'frequency' }],
+    "columnDefs": [
+      {
+        "render": function (data, type, row) {
+          return `
+               <div>
+               <button class="btn btn-primary"> Edit	</button>  
+               <button class="btn btn-primary"> Disable	</button>              
+              </div>
+              `
+        },
+        "targets": 3
+      },
+      { "targets": 0 }
+    ],
+    responsive: true
+  }
+
   ngOnInit() {
   }
 
