2092a89291a168b27b5dbfe1359ff822dbfdaf26
[anamikoye] <[anamikoye52@gmail.com]>
2017-01-20 13:31:52 +0300

    Others

--- src/app/settings/others/transaction-types/transaction-types.component.ts
+++ src/app/settings/others/transaction-types/transaction-types.component.ts
@@ -9,6 +9,32 @@ export class TransactionTypesComponent implements OnInit {
 
   constructor() { }
 
+  tableOptions: Object = {
+    colReorder: true,
+    ajax: 'assets/api/tables/others.dummy.json',
+    columns: [{ data: 'transaction' }, { data: 'description' }, { data: 'effect' }],
+    "columnDefs": [
+      {
+        // The `data` parameter refers to the data for the cell (defined by the
+        // `data` option, which defaults to the column being worked with, in
+        // this case `data: 0`.
+        "render": function (data, type, row) {
+          return `
+               <div>
+               <button class="btn btn-primary"> Edit	</button>  
+               <button class="btn btn-danger"> Disable	</button>               
+              </div>
+              `
+          // return '<a class="btn btn-primary btn-xs" href="patients/dispense/' + row['id'] + '">Dispense</a> <a class="btn btn-primary btn-xs" href="patients/view/' + row['id'] + '">Detail</a>'
+        },
+        // NOTE: Targeting the [actions] column.
+        "targets": 3
+      },
+      { "targets": 0 }
+    ],
+    responsive: true
+  }
+
   ngOnInit() {
   }
 
